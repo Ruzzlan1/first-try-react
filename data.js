b74d401e268ef4e1a2223a26f2e7cb25798168ee
Ruzzlan <voramzoram1@gmail.com>
2022-02-01 23:32:33 +0400

    "React file modified for data files using map method"

---
+++ data.js
@@ -0,0 +1,12 @@
+export const portfolio = [
+  {
+    title: 'About',
+    content:
+      'I am frontend developer with a particular interest in making things simple and automating daily tasks.I try to my best every day.Sometimes not my best :d. But that is life.Everyone has problems,then fuck it :)',
+  },
+  {
+    title: 'Interests',
+    content:
+      'I just typo nerd guy doing nerdy things :d.I playing games a lot.I love photographing,and also i do sports (dont lie)',
+  },
+]
